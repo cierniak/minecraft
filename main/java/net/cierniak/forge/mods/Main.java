915ba1523b66bf4eb1927c402c7405f45128e8d2
Michal Cierniak <cierniak@users.noreply.github.com>
2015-06-23 16:17:18 -0700

    Added file missed in previous commit.

---
+++ main/java/net/cierniak/forge/mods/Main.java
@@ -0,0 +1,20 @@
+package net.cierniak.forge.mods;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "mymod";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK 2 >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
